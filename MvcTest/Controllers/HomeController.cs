2e3332b2679b28e67ffdd8417e02d85e88cd21d5
yang <yangningyuan@outlook.com>
2015-11-25 11:33:38 +0800

    master第一次提交供分支使用

---
+++ MvcTest/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using MvcTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.tagname = "账号：";                                                    
+            //ViewBag["tagname"] = "账号：";
+            ViewData["tagpwd"] = "密码：";
+        
+            User u = new User();
+            u.Id = 1;
+            u.Username = "yangningyuan";
+            u.Password = "123456";
+            ViewBag.user = u;
+            return View();
+        }
+        /// <summary>
+        /// 登录
+        /// </summary>
+        public void login() 
+        {
+            string n = Request.Form["name"];
+            string pwd = Request.Form["password"];
+        }
+
+        public ActionResult jubupage() 
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MvcTest/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcTest.Models
+{
+    public class User
+    {
+        public int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string username;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
